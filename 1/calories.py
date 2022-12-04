a20bbfefccada0f7bbedef81fd11c87b659ab4fd
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-12-04 20:00:59 +0100

    calories completed

--- 1/calories.py
+++ 1/calories.py
@@ -2,13 +2,20 @@ file = open("input.txt", "r")
 lines = file.readlines()
 
 calories = 0
-ElfNum = 0
-
-
-help(lines)
+ElfNum = 1
+MostCalories = 0
 
 for line in lines:
 	if line.strip():
-		print ('space')
+		calories += int(line)
 	else:
-		print('num')
+
+		if calories > MostCalories:
+			MostCalories = calories
+			print ('Most cal now', MostCalories, 'on Elf', ElfNum)
+
+		print ('Elf number:', ElfNum, 'has', calories)
+		ElfNum += 1
+		calories = 0
+
+print ('Winner', MostCalories)
