4a77bc55342dac93e35f987399a2add12be2bb08
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-12-04 19:40:02 +0100

    calories

--- 1/calories.py
+++ 1/calories.py
@@ -1,1 +1,14 @@
-hello
+file = open("input.txt", "r")
+lines = file.readlines()
+
+calories = 0
+ElfNum = 0
+
+
+help(lines)
+
+for line in lines:
+	if line.strip():
+		print ('space')
+	else:
+		print('num')
