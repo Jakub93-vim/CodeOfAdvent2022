7d46bc414e66cff57876c679e67044045c76ac8e
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-12-05 10:51:52 +0100

    First three together

--- 1/calories.py
+++ 1/calories.py
@@ -4,7 +4,9 @@ lines = file.readlines()
 calories = 0
 ElfNum = 1
 MostCalories = 0
+CaloriesSum = []
 
+# the most calories
 for line in lines:
 	if line.strip():
 		calories += int(line)
@@ -12,10 +14,16 @@ for line in lines:
 
 		if calories > MostCalories:
 			MostCalories = calories
-			print ('Most cal now', MostCalories, 'on Elf', ElfNum)
+			#print ('Most cal now', MostCalories, 'on Elf', ElfNum)
 
-		print ('Elf number:', ElfNum, 'has', calories)
+		#print ('Elf number:', ElfNum, 'has', calories)
+		CaloriesSum.append(calories)
 		ElfNum += 1
 		calories = 0
 
 print ('Winner', MostCalories)
+
+#first three biggest amounts of calories
+
+FirstThree = sorted(CaloriesSum, reverse = True)[:3]
+print('First three together', sum(FirstThree))
