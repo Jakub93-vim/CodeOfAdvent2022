06f69c111c2e9b20e1552137a8127c985d6f7324
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-12-07 11:17:10 +0100

    part two

--- 2/score.py
+++ 2/score.py
@@ -11,42 +11,41 @@ score = 0
 
 for line in lines:
 
-	if line[0] == "A" and line[1] == "X":
-		print ('A and X')
-		score += 1
+	if line[0] == "A" and line[2] == "X":
+		score += 4
 
-	elif line[0] == "A" and line[1] == "Y":
-		
-		score += 7
-
-	elif line[0] == "A" and line[1] == "Z":
-		
-		score += 3
-
-	elif line[0] == "B" and line[1] == "X":
-
-		score += 2
-
-	elif line[0] == "B" and line[1] == "Y":
-		
-		score += 5
-
-	elif line[0] == "B" and line[1] == "Z":
+	elif line[0] == "A" and line[2] == "Y":
 		
 		score += 8
 
-	elif line[0] == "C" and line[1] == "X":
-
-		score += 9
-
-	elif line[0] == "C" and line[1] == "Y":
+	elif line[0] == "A" and line[2] == "Z":
 		
 		score += 3
 
-	elif line[0] == "C" and line[1] == "Z":
+	elif line[0] == "B" and line[2] == "X":
+
+		score += 1
+
+	elif line[0] == "B" and line[2] == "Y":
+		
+		score += 5
+
+	elif line[0] == "B" and line[2] == "Z":
+		
+		score += 9
+
+	elif line[0] == "C" and line[2] == "X":
+
+		score += 7
+
+	elif line[0] == "C" and line[2] == "Y":
+		
+		score += 2
+
+	elif line[0] == "C" and line[2] == "Z":
 		
 		score += 6
 
-	#print ('Score on the way', score)
+	print ('Score on the way', score)
 
 print ('Final score is', score)
