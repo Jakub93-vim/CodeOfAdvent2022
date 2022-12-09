b6d5178e05f24a79f198119c46e00efde97f76d7
Jakub Kasal <74777800+Jakub93-vim@users.noreply.github.com>
2022-12-09 10:46:40 +0100

    first part completed!

--- 3/items.py
+++ 3/items.py
@@ -6,25 +6,40 @@ lines = file.readlines()
 
 BigLetters = {}
 smallLetters = {}
+score = 0
 
 for L, N in zip(ascii_uppercase, range(27,53)):
-
 	BigLetters.update({L:N})
 
 for l, N in zip(ascii_lowercase, range(1,28)):
-
 	smallLetters.update({l:N})
 
-for line in lines:
+def updateScore (x):
 
+	global score 
+	if x.isupper():
+		score += BigLetters[x]
+
+	else:
+		score += smallLetters[x]
+	return score
+
+for line in lines:
 	First_Part = line[:len(line)//2]
 	Second_Part = line[len(line)//2:]
 
 	for x in First_Part:
 		for y in Second_Part:
-
 			if x == y:
-				print ('The same letter here', x)
+				updateScore(x)
+				print ('The same letter here', x, 'and score is', score)
+				break
+		else:
+			continue
+		break
+
+
+
 
 	print ( line, First_Part, Second_Part)
 
